54e3b23ff870a1a5b2c111ca728e36e27094db2a
Володимир Сологубов <wova607@hotmail.com>
2019-03-03 13:22:32 +0200

    add name

--- GitTest/WpfGitTastApp/MainWindow.xaml.cs
+++ GitTest/WpfGitTastApp/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace WpfGitTastApp
             list.Add(new User { Name = "Vasya", Login = "Vasilij", Password = "qwerty" });
             list.Add(new User { Name = "Srek", Login = "Srekovich", Password = "qwerty2" });
             list.Add(new User { Name = "Olga", Login = "Olga", Password = "qwerty3" });
+            
             //foreach (var item in list)
             //{
             //    MessageBox.Show((item as User).Name);
